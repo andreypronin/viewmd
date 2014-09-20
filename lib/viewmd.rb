a0ca49afadc8b97cb5e5e190d4664d3aadff6c1b
Andrey Pronin <moonfly.msk@gmail.com>
2014-09-20 14:18:06 -0400

    Fixed processing

--- lib/viewmd.rb
+++ lib/viewmd.rb
@@ -1,12 +1,17 @@
 require 'viewmd/version'
 require 'github/markup'
+require 'launchy'
 require 'tempfile'
 
 module Viewmd
-  def view(name)
+  def self.view(name)
     Tempfile.create(['viewmd','.htm'],Dir.tmpdir,File::CREAT|File::TRUNC|File::RDWR, 0600) do |f|
-      GitHub::Markup.render(f.path, File.read(name))
-      Launchy.open(f.path)
+      f << GitHub::Markup.render(name, File.read(name))
+      f.flush
+      Launchy.open(f.path) 
+      # joining the launchy thread doesn't help to wait for browser to close before deleting the file
+      # so, let's just wait a little
+      sleep 2
     end
   end
 end
